12bd4084e01b67050cf55e36f79b5d09f907e0f1
rashidmohd <rashidmohammed.pp@gmail.com>
2016-10-06 11:50:10 +0400

    Assignment Test

---
+++ assignment1-starter-code/app.js
@@ -0,0 +1,43 @@
+(function(){
+'use strict';
+angular.module('LCApp',[])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.Lunch="";
+  $scope.Result="";
+  $scope.Color="Blue";
+
+  $scope.CheckLunch= function(){
+  var TotalItem = 0;
+   if ($scope.Lunch !=""){
+
+      var Lunch=$scope.Lunch.split(",");
+
+     for (var i=0; i < Lunch.length; i++){
+
+    if ( (Lunch[i].trim())!=""){
+
+    TotalItem++;
+    //  alert(TotalItem);
+  }
+}
+if (TotalItem>3){
+  $scope.Color="Red";
+  $scope.Result=   "Too much!";
+}
+else{
+    $scope.Color="Green";
+    $scope.Result=   "Enjoy!";
+}
+    alert(TotalItem);
+   }
+   else {
+     $scope.Color="Black";
+     $scope.Result=   "Please enter data first";
+   }
+
+//  $scope.Result=Lunch[1];
+};
+}
+})();
